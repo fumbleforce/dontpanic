22276445c6541150ee2e039b6ed70d26ff9aebeb
jorgeer <jfemail3@gmail.com>
2013-02-13 15:25:13 +0100

    simple message passing with socket.io

--- client/rec/js/client.js
+++ client/rec/js/client.js
@@ -1,4 +1,13 @@
-function w(){
-    document.write("text");
-};
-w();
+var socket = io.connect('http://localhost');
+
+socket.on('isconnected', function () {
+    console.log('Connected');
+    socket.emit('msg', 'Hello server');
+});
+
+socket.on('msg', function (msg) {
+    console.log(msg);
+    socket.emit('msg', 'You said ' + msg + ' to me.');
+});
+
+
--- server.js
+++ server.js
@@ -1,18 +1,69 @@
-var express = require('express'),
-    server = module.exports = express(),
-    engine = require('./server/js/engine.js');
+//Variables and import
+var express     = require('express'),
+    server      = module.exports = express(),
+    engine      = require('./server/js/engine.js');
+    ioserver    = require('http').createServer(server);
 
 
 //Configuration
-
 server.engine('.html', require('ejs').__express);
 server.set('views', __dirname + '/client/views');
 server.use(express.static(__dirname + '/client/rec'));
 server.set('view engine', 'html');
+server.set('port', process.env.PORT || 8008);
 
+
+//Handle requests
 server.get('/', function(request, response){
-  engine.handle_request(request, response);
+    response.render('index');
 })
-server.listen(8008);
-
 console.log('Server running at http://127.0.0.1:8008/');
+
+
+// Configure Socket.IO
+ioserver.listen(server.get('port'));
+var socket_listener = require('socket.io').listen(ioserver);
+
+/*
+socket_listener.configure(function (){
+    socket_listener.set('log level', 0);
+    socket_listener.set('authorization', function (handshakeData, callback) {
+        callback(null, true);
+    });
+});
+*/
+
+// Handle client interaction    
+socket_listener.sockets.on('connection', function (client) {
+
+    //Client setup
+    client.userid = 1;
+    client.emit('isconnected');
+    //engine.start_game(client);
+    console.log('**SOCKET_LISTENER** client ' + client.userid + ' connected');
+            
+    //Client said hello
+    client.on('msg', function(msg) {
+        console.log('**SOCKET_LISTENER** received message: '+ msg);
+        if(msg === 'Hello server'){
+            client.emit('msg', 'Oh hello there Client');
+        }
+    });     
+            
+    //Client sends command
+    client.on('command', function(c) {
+        console.log('**SOCKET_LISTENER** received command ' + c);
+        engine.command(client, c);
+    });
+
+    //Client Disconnects
+    client.on('disconnect', function () {
+        console.log('**SOCKET_LISTENER** client ' + client.userid + ' disconnected.');
+        //engine.endGame(client.game.id, client.userid);
+    });       
+});// end onConnection
+    
+   
+    
+    
+    
