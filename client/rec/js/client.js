c8d4616b4510cedf9a2928865689e42c75e16402
jorgeer <jfemail3@gmail.com>
2013-02-15 14:59:50 +0100

    oog command handling

--- client/rec/js/client.js
+++ client/rec/js/client.js
@@ -1,5 +1,6 @@
 var socket = io.connect('http://localhost');
 
+
 socket.on('isconnected', function () {
     console.log('Connected');
     socket.emit('msg', 'Hello server');
@@ -11,16 +12,69 @@ socket.on('msg', function (msg) {
 });
 
 socket.on('data', function (data) {
+    var d = JSON.parse(data);
     console.log('Received data ' + data);
-    //game_client.onData(data);
+    //game_client.onData(d);
 });
 
 function command(c){
-    console.log('Sending command "' + c+ '"');
-    socket.emit('command',c);
+    var send = JSON.stringify(c)
+    console.log('Sending command "' + send + '"');
+    socket.emit('command', send);
 }
 
 function msg(m){
     console.log('Sending message "' + c + '"');
     socket.emit('msg', m);
 }
+
+
+
+
+// Testing ground
+
+var Player = {
+    title : {},
+    health : 0
+}
+
+var Title = {
+    title : 'Lord of the Rings'
+}
+
+var playerFunction = function(age, stuff){
+    this.age = age;
+    this.stuff = stuff;
+}
+playerFunction.prototype.add = function(){
+    return this.age + this.stuff;
+}
+
+var p = new playerFunction(2, 15);
+
+if (typeof Object.beget !== 'function') {
+    Object.beget = function (o) {
+        var F = function () {};
+        F.prototype = o;
+        return new F();
+    };
+}
+
+
+var test = function(){
+    var newplayer = Object.beget(Player);
+    newplayer.title = Title;
+    console.log(newplayer.title.title);
+    var jsontext = JSON.stringify(newplayer);
+    console.log(jsontext);
+    var stringtext = JSON.parse(jsontext);
+    console.log(stringtext);
+    console.log(p);
+    var functiontext = JSON.stringify(p);
+    console.log(functiontext);
+    var functionjson = JSON.parse(functiontext);
+    console.log(functionjson);
+    console.log(p.add());
+    console.log(functionjson.age + functionjson.stuff);
+}
+test();
