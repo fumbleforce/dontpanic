7ae0ed311ade1abe05b3cc3bcd73fc4502ef7d12
Sindre Svendsrud <sindre.svendsrud@gmail.com>
2013-03-08 13:35:47 +0100

    begynt på queries til databasen

--- database.js
+++ database.js
@@ -18,9 +18,7 @@ var connection = mysql.createConnection({
 connection.connect(function(err) { 
     if (err) {
         console.log('ERROR: ' + err);
-    } else {
-        console.log('Connected to Server');
-    };
+    }
 });
 
 /* Query Test */
@@ -29,8 +27,68 @@ connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
   console.log('The solution is: ', rows[0].solution);
 });
 
+var test_query = function () {
+	connection.query('SELECT text AS solution FROM test WHERE ID = 1', 
+	function(err, rows, fields) {
+ 		if (err) throw err;
+ 		return rows[0].solution;
+	});
+}
 
+/*
 
+get_password checks if the password parameter matches the database password for the 
+given user, callback on correct_password or wrong_password 
+
+*/
+
+var get_password = function (username, password, callback) {
+	connection.query('SELECT Password AS solution FROM User WHERE ID = 1', 
+	function(err, rows, fields) {
+		if (err) throw err;
+		if (rows[0].solution === password) {
+			callback.correct_password(username);
+		}		
+		else {
+			callback.wrong_password(username);
+		}
+	});
+}
+
+/* for å teste sql spørringene */
+
+function caller () {
+    this.correct_password = function(username) {
+        console.log(username);
+    };
+    function wrong_password (username) {
+    	console.log(username);
+    }
+    function wrong_password() {
+    	console.log("lol");
+    }
+   	this.wrong_password = function () {
+   		console.log("ad");
+  	};
+   	
+    this.wrong_password = function (username) {
+    	console.log(username);
+    };
+}
+
+get_password('dontpanic', '1243', caller);
+
+/*
+	
+*/
+
+/*var get_replay_state = function (game_id, turn_id, callback) {
+	connection.query('SELECT State_array AS solution FROM Replay WHERE Game_ID = ' + game_ID 
+	'AND Turn_ID = ' + turn_id', function (err, rows, fields) {
+		if (err) throw err;
+		callback(rows[0].solution);
+	} 
+}*/
 
 function handleDisconnect(connection) {
   connection.on('error', function(err) {
@@ -49,7 +107,6 @@ function handleDisconnect(connection) {
     connection.connect();
   });
 }
-
 handleDisconnect(connection);
 
 
