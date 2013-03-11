2052005d78b92369ce5093e0b94f47fe78d8668b
Sindre Svendsrud <sindre.svendsrud@gmail.com>
2013-03-11 13:29:50 +0100

    sql queries begynner å komme

--- database.js
+++ database.js
@@ -38,19 +38,20 @@ var test_query = function () {
 /*
 
 get_password checks if the password parameter matches the database password for the 
-given user, callback on correct_password or wrong_password 
+given user, callback on correct_password or wrong_password, callback fungerer ikke nå, men 
+tror selve kallet fungerer
 
 */
 
 var get_password = function (username, password, callback) {
-	connection.query('SELECT Password AS solution FROM User WHERE ID = 1', 
-	function(err, rows, fields) {
+	connection.query('SELECT Password AS solution FROM User WHERE User_Name = ' + 
+	connection.escape(username), function(err, rows, fields) {
 		if (err) throw err;
 		if (rows[0].solution === password) {
 			callback.correct_password(username);
 		}		
 		else {
-			callback.wrong_password(username);
+			callback.wrong_password();
 		}
 	});
 }
@@ -59,10 +60,10 @@ var get_password = function (username, password, callback) {
 
 function caller () {
     this.correct_password = function(username) {
-        console.log(username);
+        console.log('correct password for', username);
     };
     function wrong_password (username) {
-    	console.log(username);
+    	console.log('wrong password for', username);
     }
     function wrong_password() {
     	console.log("lol");
@@ -76,20 +77,53 @@ function caller () {
     };
 }
 
-get_password('dontpanic', '1243', caller);
+//get_password('dontpanic', '1243', caller);
 
 /*
-	
+	Sends a callback with the replay state for the given game id and turn id
 */
 
 /*var get_replay_state = function (game_id, turn_id, callback) {
-	connection.query('SELECT State_array AS solution FROM Replay WHERE Game_ID = ' + game_ID 
-	'AND Turn_ID = ' + turn_id', function (err, rows, fields) {
+	connection.query('SELECT State_array AS solution FROM Replay WHERE Game_ID = ' + 
+	game_ID, 'AND Turn_ID = ' turn_id, function (err, rows, fields) {
 		if (err) throw err;
 		callback(rows[0].solution);
 	} 
 }*/
 
+/* 
+	Puts the texttest in the table test
+*/
+var test = function (texttest) {
+	connection.query('INSERT INTO test SET ?', {text: texttest}, function(err, result) {
+  	if (err) throw err;
+  	console.log(result.insertId);
+});
+
+}
+
+/* 
+	Puts the given replay string to Replay in the database  
+*/
+var set_replay_state = function (game_id, turn_id, replay_string)  {
+	connection.query('INSERT INTO Replay SET?', {Game_ID: game_id, 
+	Turn_id: turn_id, State_array: replay_string}, function (err, result) {
+		if (err) throw err;
+		console.log('Sucessfully added', replay_string, 'to replay state');
+	});
+}
+/* get if User admin*/
+
+var is_user_admin = function (username, callback) {
+	connection.query('SELECT Is_Admin AS solution FROM User WHERE User_Name = ' + 
+	connection.escape(username), function(err, rows, fields) {
+		if (err) throw err;
+		callback(rows[0].solution);
+	});
+}
+/* get Username */ 
+
+/* */
 function handleDisconnect(connection) {
   connection.on('error', function(err) {
     if (!err.fatal) {
