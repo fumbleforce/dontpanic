e777f1f1a89e8e50135e52227666f24731f5f1c9
Forestviolence <forestviolence@gmail.com>
2013-04-08 14:57:40 +0200

    delete nodes and create connections between nodes

--- expert_interface.js
+++ expert_interface.js
@@ -84,7 +84,10 @@ var gco = {
     cst : {},
     turn : 0,
     active_player : 0,
-	next_node : 1
+	next_node : 0,
+	mode : "add node",
+	selected_node : -1,
+	connection : -1
 }
 gco.ctx = gco.canvas.getContext("2d");
 
@@ -106,7 +109,7 @@ gco.init_game = function (d) {
     gco.setup_canvas();
     gco.set_canvas_listener();
 
-    
+
     gco.draw();
 
 
@@ -306,7 +309,26 @@ gco.roadblock_draw = function(node, ctx){
 	    ctx.stroke();
     }
 }
-
+gco.draw_connections = function(ctx){
+	var nodes = gco.nodes;
+	ctx.strokeStyle = '#202020';
+	ctx.lineWidth = 5;
+	
+	for(var i = 0; i < nodes.length; i++){
+		node = nodes[i];
+	
+		for(var z = 0; z < nodes[i].connects_to.length; z++){
+			ctx.beginPath();
+			ctx.moveTo(node.x, node.y);
+			
+			nodeto = nodes[node.connects_to[z].id];
+			
+			ctx.lineTo(((nodeto.x)+node.x)/2, ((nodeto.y)+node.y)/2);
+			ctx.closePath();
+			ctx.stroke();
+		}
+	}	
+}
 gco.background_draw = function(ctx){
     ctx.fillStyle="grey";
     ctx.fillRect(0,0, c_width, c_height);
@@ -463,6 +485,8 @@ gco.draw = function(){
         players = gco.players;
         
     gco.background_draw(ctx);    
+	
+	gco.draw_connections(ctx);
         
     for (var i = 0; i < zones.length; i++) {
         zone = zones[i];
@@ -503,6 +527,52 @@ gco.draw = function(){
     
 }// end draw
 
+gco.create_connection = function(){
+	if((gco.connection > -1) && (gco.selected_node > -1) && (gco.connection != gco.selected_node)){
+		gco.nodes[gco.connection].connects_to.push(gco.nodes[gco.selected_node]);
+		gco.nodes[gco.selected_node].connects_to.push(gco.nodes[gco.connection]);
+		
+		console.log("connection between " + gco.selected_node + " and " + gco.connection);
+		
+		gco.connection = -1;
+		
+	}
+		
+	
+}
+gco.node_connection = function(){
+
+	gco.connection = gco.selected_node; 
+	
+}	
+
+gco.del_selected_node = function(){ // deletes the selected node, if none is selected nothing will be removed
+	
+	if(gco.selected_node > -1){
+		index = gco.selected_node;
+		node = gco.nodes[index];
+		
+		for (var i = 0; i < gco.nodes[index].connects_to.length; i++){
+			cnode = gco.nodes[index].connects_to[i];
+			index2 = cnode.connects_to.indexOf(node);
+			cnode.connects_to.splice(index2, 1);
+			
+		}
+		
+		gco.nodes.splice(index, 1);
+
+		
+		gco.selected_node = -1;
+		gco.re_id();
+		gco.draw();
+	}
+}
+
+gco.re_id = function(){ // redo all id's of the nodes, to make it look better
+	for(gco.next_node = 0; gco.next_node < gco.nodes.length; gco.next_node++){
+		gco.nodes[gco.next_node].id = gco.next_node;
+	}
+}
 
 gco.set_canvas_listener = function(){
     var canvas = gco.canvas,
@@ -515,28 +585,19 @@ gco.set_canvas_listener = function(){
     canvas.addEventListener('selectstart', function(e) { e.preventDefault(); return false; }, false);
 
     canvas.addEventListener('mousedown', function(e) {
-		console.log();
+		
         
         var mx = e.offsetX,
             my = e.offsetY,
             selected;
 
-		console.log(mx + "   adfg" + my);
+		
         
         cst.selected_zone = null;
         cst.selected_node = null;
-        
-		gco.nodes.push(newNode = {
-                id:gco.next_node,
-                x:mx,
-				y:my,
-                is_start_position:false,
-				connects_to:[]
-            });
-		gco.next_node++;
-		
-	    /*
-        if (cst.selection) {
+       
+		/*
+		if (cst.selection) {
             console.log("clearing selection");
             cst.selection.x = nodes[cst.selection.node].x
             cst.selection.y = nodes[cst.selection.node].y
@@ -544,8 +605,8 @@ gco.set_canvas_listener = function(){
             cst.dragging = false;
             gco.draw();
         }
-
-        for (var i = 0; i < players.length; i++) {
+		/*
+		for (var i = 0; i < players.length; i++) {
 
         	if (gco.player_contains(players[i], mx, my)) {
         		console.log("Clicked on a player "+players[i].id);
@@ -564,19 +625,45 @@ gco.set_canvas_listener = function(){
         		}
         	}
         }
+		*/
+		for (var i = 0; i < gco.nodes.length; i++) {
 
-        for (var i = 0; i < zones.length; i++) {
-
-        	if (gco.zone_contains(zones[i], mx, my)) {
-        		console.log("Clicked on zone "+i);
-        		cst.selected_zone = i;
-        		//TODO for testing, we add 'decrease_panic' when selecting zones
-        		command('decrease_panic', {zone_id : cst.selected_zone});
+        	if (gco.node_contains(nodes[i], mx, my)) {
+        		console.log("Clicked on node "+i);
+				cst.selected_node = i;
+				gco.selected_node = i;
+				
+				if(gco.connection > -1){
+					gco.create_connection();
+					
+				}
+				
+				
+				selected = nodes[i];
+				
+				cst.dragoffx = mx - nodes[i].x;
+				cst.dragoffy = my - nodes[i].y;
+				cst.dragging = true;
+				cst.selection = selected;
+				
+				
         		gco.draw();
         		return;
            }
         }
-		*/
+		
+		
+		
+		gco.nodes.push(newNode = {
+				id:gco.next_node,
+				x:mx,
+				y:my,
+				is_start_position:false,
+				connects_to:[]
+			});
+		gco.next_node++;
+		
+
         gco.draw();
         
     }, true);//end mousedown listener
@@ -597,14 +684,11 @@ gco.set_canvas_listener = function(){
             my = e.offsetY;
              
         if (cst.dragging && cst.selection !== undefined) {
-            console.log("Mouse let go of player");
+            console.log("Mouse let go of node");
             for (var i = 0; i < nodes.length; i++) {
                 if (gco.node_contains(nodes[i], mx, my)) {
                     
-                    command('move_player', {
-                        player_id : cst.selection.id,
-                        node_id : i
-                    });
+                    
                     cst.selection = undefined;
                     cst.dragging = false;
                     gco.draw();
