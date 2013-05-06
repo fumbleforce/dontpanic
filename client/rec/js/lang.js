5ada0eb8a2ca5cbe6e4c0a3b8b4c1310343544e6
jorgeer <jfemail3@gmail.com>
2013-05-06 14:02:14 +0200

    commented lang and client

--- client/rec/js/lang.js
+++ client/rec/js/lang.js
@@ -1,17 +1,26 @@
 
 
-// The default language
+/** 
+* The chosen language, default set to English
+*
+* @property chosen_lang
+*/
 var chosen_lang = "en";
 
-// Supported languages
+/** 
+* List of supported languages
+*
+* @property supported_lang
+*/
 var supported_lang = ['en', 'no', 'it'];
 
 
-/* Object containing all labels.
-
- Change the text after the colon ( : ) 
- to change the associated label.
-
+/** 
+* Object containing all labels.
+* Change the text after the colon ( : ) 
+* to change the associated label.
+*
+* @property lang
 */
 var lang = {
 	
@@ -145,13 +154,92 @@ var lang = {
 		'default-template' : 'Standard mal',
 		'no-active-rooms' : 'Ingen aktive rom!',
 		'no-avail-replay' : 'Ingen tilgjengelige repriser!',
+		
+		// Errors
+		'player-not-adj' : "Spiller er ikke i nærheten av sonen",
+		'not-enough-people' : "Ikke nok folk i sonen",
+		'panic-too-low' : "Panikken er for lav til å senke",
+		'not-adj-node' : "Noden er ikke i nærheten",
+		'need-player-node' : "Trenger en spiller til på noden",
+		'no-rb' : "Ingen veiblokkering i noden",
+		'node-has-rb' : "Noden har allerede en blokkering",
+		'player-not-node' : "Spilleren er ikke på denne noden",
+		'has-info' : "Noden har allerede et info senter",
+		'node-not-conn' : 'Noden er ikke koblet til denne',
+		'player-lacks-action' : "Spilleren har ikke nok handlinger",
+		'move-same' : "Kan ikke flytte til samme node"
 	},
 	
 	
 	
 	// Italian
 	'it': {
+		'none' : ' ',
 	
+		// Button Labels
+		'end-game-label' : 'End Game',
+		'player-turn-label' : ['Player ','s turn'],
+		'turn-label' : 'Turn: ',
+		'action-label' : 'Action points left: ',
+		'timer-label' : 'Panic increase in: ',
+		'player' : 'Player ',
+		
+		// Game related
+		'role_desc' : {
+			'crowd manager':'The Crowd Manager can decrease panic by 10 instead of 5',
+			'operation expert':'The Operation Expert can build and remove roadblocks alone',
+			'driver':'The Driver can move 10 people between zones instead of 5',
+		},
+		'role_name' : {
+			'crowd manager':'Crowd Manager',
+			'operation expert':'Operation Expert',
+			'driver':'Driver',
+		},
+		
+		// Actions
+		'op-add-road-block' : 'Add road block',
+		'op-info-center' : 'Add information center',
+		'op-dec-panic' : 'Decrease panic',
+		'op-move-people' : 'Move people',
+		'op-rem-block' : 'Remove road block',
+		'op-next-player' : 'Next player',
+		
+		
+		// Status updates
+		'stat-move-people' : 'Moving people from zone ',
+		'stat-click-player' : 'Clicked on player ',
+		'stat-select-node' : 'Selected node ',
+		'stat-select-zone' : 'Selected zone ',
+		'stat-moved-people' : 'Moved people to zone ',
+		'stat-dragging-player' : 'Dragging player ',
+		
+		
+		// Index
+		'play' : 'Play',
+		'expert-interface' : 'Expert Interface',
+		'game-master' : 'Game Master',
+		'replay' : 'Replay',
+		
+		'footer' : 'A game by Group 10',
+		'title' : "Don't Panic",
+	
+		'default-template' : 'Default template',
+		'no-active-rooms' : 'No active rooms!',
+		'no-avail-replay' : 'No available replays!',
+		
+		// Errors
+		'player-not-adj' : "Player is not adjacent to zone",
+		'not-enough-people' : "Not enough people in zone",
+		'panic-too-low' : "The panic is too low to decrease",
+		'not-adj-node' : "The node is not adjacent",
+		'need-player-node' : "Need another player on node",
+		'no-rb' : "No road block on this node",
+		'node-has-rb' : "Node already has road block",
+		'player-not-node' : "Player is not on this node",
+		'has-info' : "Node already has info center",
+		'node-not-conn' : 'Node is not connected',
+		'player-lacks-action' : "Player doesn't have enough actions",
+		'move-same' : "Cannot move to the same node"
 	},
 }
 
@@ -169,7 +257,14 @@ var lang = {
 
 
 
-
+/**
+* Translates the given language to chosen language.
+* Defaults firsts to the english translation,
+* then to "Label not defined"
+*
+* @method speak
+* @param {String} label String to be translated
+*/
 function speak(label){
 	var w = lang[chosen_lang][label];
 	if (!w || w === 'undefined' || w === undefined) {
@@ -181,6 +276,12 @@ function speak(label){
 	return w;
 }
 
+/**
+* Changes the language
+*
+* @method set_lang
+* @param {String} l Language
+*/
 function set_lang(l){
 	if(supported_lang.indexOf(l) === -1){
 		chosen_lang = 'en';
